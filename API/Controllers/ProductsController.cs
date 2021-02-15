a1ca81e714c8bbe92729149f8f551b83e7a4b756
sabaoth <sabaoth@ukr.net>
2021-02-15 23:53:19 +0200

    all

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+
+        public string GetProducts()
+        {
+            return "This will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct()
+        {
+            return "Single product";
+        }
+    }
+}
